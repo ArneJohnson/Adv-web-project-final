35827c0dd862b193f55b54b5a772a27b1c5b433e
ArneJohnson <arnejohnson02@gmail.com>
2025-03-11 00:26:19 +0100

    login

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const cookieParser = require('cookie-parser');
+const crypto = require('crypto');
 const port = 5000;
+
+const SECRET = 'mySecretCookieToken';
+
+const sessions = {};
+
+app.use(cookieParser(SECRET));
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
@@ -28,6 +36,30 @@ function readStores() {
 function writeStores(stores) {
     fs.writeFileSync(storesFilePath, JSON.stringify(stores, null, 2));
 }
+
+// Login
+app.get('/login', (req, res) => {
+    res.send(`<!DOCTYPE html><html><head><title>Login</title></head><body><h1>Login</h1>
+<form method="POST" action="/login">
+<label for="username">Username:</label>
+<input type="text" name="username" id="username" required /><br/><br/>
+<label for="password">Password:</label>
+<input type="password" name="password" id="password" required /><br/><br/>
+<button type="submit">Login</button>
+</form><p><a href="/">Home</a></p></body></html>`);
+});
+
+app.post('/login', express.urlencoded({ extended: true }), (req, res) => { //POST /login route
+    const { username, password } = req.body;
+    if (username === 'admin' && password === 'password') { // In a real-world validate credentials against a database
+        const token = crypto.randomBytes(64).toString('hex'); // Generate a secure random token
+        sessions[token] = { username }; // Store the token along with user data in our session store
+        res.cookie('authToken', token, { signed: true, httpOnly: true }); // Set a signed, HTTP-only cookie with the token
+        res.redirect('/'); // Redirect the user to the default route after successful login
+    } else {
+        res.status(401).send(`Login Error: Invalid credentials. Please try again.`);
+    }
+});
 
 // GET endpoint to fetch all stores
 app.get('/api/stores', (req, res) => {
